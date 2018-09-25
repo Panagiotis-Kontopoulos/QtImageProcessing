ae9268c253f2642f49926071fbc17a5de8da9857
Panagiotis Kontopoulos <kls.panagiotis@gmail.com>
2018-09-25 17:04:46 +0300

    Menu bar and triggers for items in this bar

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,6 +6,7 @@
 #include <QLabel>
 #include <QDir>
 #include <iostream>
+#include <QMessageBox>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -28,8 +29,6 @@ MainWindow::MainWindow(QWidget *parent) :
         std::cout<<w;
         std::cout<<h;
         ui->image_label->setPixmap(img.scaled(w,h,Qt::KeepAspectRatio));
-
-
     }
 
 }
@@ -37,4 +36,140 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_actionOpen_Image_triggered()
+{
+    QMessageBox::information(this,"Open","Choose File to open");
+
+}
+
+void MainWindow::on_actionSave_Image_triggered()
+{
+    QMessageBox::information(this,"Save","Save File");
+}
+
+void MainWindow::on_actionEnable_Auto_Save_triggered()
+{
+    QMessageBox::information(this,"Auto Save","Auto Save Enabled");
+}
+
+void MainWindow::on_actionDisable_Auto_Save_triggered()
+{
+    QMessageBox::information(this,"Auto Save","Auto Save Disabled");
+}
+
+void MainWindow::on_actionExit_triggered()
+{
+    QApplication::quit();
+}
+
+void MainWindow::on_actionMethod_Input_triggered()
+{
+    QMessageBox::information(this,"Input","Input Methods");
+}
+
+void MainWindow::on_actionMore_triggered()
+{
+    QMessageBox::information(this,"More","More Options");
+}
+
+void MainWindow::on_actionGasussian_Noise_triggered()
+{
+    QMessageBox::information(this,"Gaussian","Add Gaussian Noise");
+}
+
+void MainWindow::on_actionRandom_Noise_triggered()
+{
+    QMessageBox::information(this,"Random","Add Random Noise");
+}
+
+void MainWindow::on_actionAll_White_triggered()
+{
+
+}
+
+void MainWindow::on_actionAll_Black_triggered()
+{
+
+}
+
+void MainWindow::on_actionAll_White_2_triggered()
+{
+
+}
+
+void MainWindow::on_actionAll_Black_2_triggered()
+{
+
+}
+
+void MainWindow::on_actionComparison_triggered()
+{
+
+}
+
+void MainWindow::on_actionEvaluation_triggered()
+{
+
+}
+
+void MainWindow::on_actionAll_3x3_triggered()
+{
+
+}
+
+void MainWindow::on_actionCross_3x3_triggered()
+{
+
+}
+
+void MainWindow::on_actionAll_5x5_triggered()
+{
+
+}
+
+void MainWindow::on_actionRhombus_5x5_triggered()
+{
+
+}
+
+void MainWindow::on_actionAll_3x3_2_triggered()
+{
+
+}
+
+void MainWindow::on_actionCross_3x3_2_triggered()
+{
+
+}
+
+void MainWindow::on_actionAll_5x5_2_triggered()
+{
+
+}
+
+void MainWindow::on_actionRhombus_5x5_2_triggered()
+{
+
+}
+
+void MainWindow::on_actionSwell_Filter_triggered()
+{
+
+}
+
+void MainWindow::on_actionShrink_Filter_triggered()
+{
+
+}
+
+void MainWindow::on_actionUndo_triggered()
+{
+
+}
+
+void MainWindow::on_actionRedo_triggered()
+{
+
 }
--- mainwindow.h
+++ mainwindow.h
@@ -15,6 +15,61 @@ public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_actionOpen_Image_triggered();
+
+    void on_actionSave_Image_triggered();
+
+    void on_actionEnable_Auto_Save_triggered();
+
+    void on_actionDisable_Auto_Save_triggered();
+
+    void on_actionExit_triggered();
+
+    void on_actionMethod_Input_triggered();
+
+    void on_actionMore_triggered();
+
+    void on_actionGasussian_Noise_triggered();
+
+    void on_actionRandom_Noise_triggered();
+
+    void on_actionAll_White_triggered();
+
+    void on_actionAll_Black_triggered();
+
+    void on_actionAll_White_2_triggered();
+
+    void on_actionAll_Black_2_triggered();
+
+    void on_actionComparison_triggered();
+
+    void on_actionEvaluation_triggered();
+
+    void on_actionRhombus_5x5_triggered();
+
+    void on_actionAll_3x3_triggered();
+
+    void on_actionCross_3x3_triggered();
+
+    void on_actionAll_5x5_triggered();
+
+    void on_actionAll_3x3_2_triggered();
+
+    void on_actionCross_3x3_2_triggered();
+
+    void on_actionAll_5x5_2_triggered();
+
+    void on_actionRhombus_5x5_2_triggered();
+
+    void on_actionSwell_Filter_triggered();
+
+    void on_actionShrink_Filter_triggered();
+
+    void on_actionUndo_triggered();
+
+    void on_actionRedo_triggered();
+
 private:
     Ui::MainWindow *ui;
 };
